fa887f41efdbf9bb23860fe17f546f4482392879
davidjumble <hellodavidparkinson@gmail.com>
2018-07-09 17:40:16 +0100

    added an info button

--- src/components/Bust.js
+++ src/components/Bust.js
@@ -12,37 +12,37 @@ class Bust extends Component {
       ["SWITZERLAND", 4],
       ["UK", 5],
       ["NETHERLANDS", 6],
-      ["Lithiania", 7],
-      ["Japan", 8],
-      ["Israel", 9],
-      ["Portugal", 10],
-      ["Belgium", 11],
-      ["Indonisia", 12],
-      ["Greece", 13],
-      ["Finland", 14],
-      ["Jamaica", 15],
-      ["Australia", 16],
-      ["Romania", 17],
-      ["India", 18],
-      ["Spain", 19],
-      ["China", 20],
-      ["Chile", 21],
-      ["Korea", 22],
-      ["Nigeria", 23],
-      ["NewZealand", 24],
-      ["Nepal", 25],
-      ["Bulgaria", 26],
+      ["LITHUANIA", 7],
+      ["JAPAN", 8],
+      ["ISRAEL", 9],
+      ["PORTUGAL", 10],
+      ["BELGIUM", 11],
+      ["INDONISIA", 12],
+      ["GREECE", 13],
+      ["FINLAND", 14],
+      ["JAMAICA", 15],
+      ["AUSTRALIA", 16],
+      ["ROMANIA", 17],
+      ["INDIA", 18],
+      ["SPAIN", 19],
+      ["CHINA", 20],
+      ["CHILE", 21],
+      ["KOREA", 22],
+      ["NIGERIA", 23],
+      ["NEWZEALAND", 24],
+      ["NEPAL", 25],
+      ["BULGARIA", 26],
       ["US", 27],
-      ["Hungary", 28],
-      ["Austria", 29],
-      ["Ghana", 30],
-      ["Kenya", 31],
-      ["Sweden", 32],
-      ["Germany", 33],
-      ["Peru", 34],
-      ["Brazil", 35],
-      ["Turkey", 36],
-      ["Italy", 37]
+      ["HUNGARY", 28],
+      ["AUSTRIA", 29],
+      ["GHANA", 30],
+      ["KENYA", 31],
+      ["SWEDEN", 32],
+      ["GERMANY", 33],
+      ["PERU", 34],
+      ["BRAZIL", 35],
+      ["TURKEY", 36],
+      ["ITALY", 37]
     ]
   };
 
@@ -64,6 +64,9 @@ class Bust extends Component {
           this.handleClick(event);
         }}
       >
+        <div className="info-button">
+          <span className="question-mark">?</span>
+        </div>
         <div className="tear-container">
           <img className="bust" src={bust} />
           <Tears tearQuantity={this.state.country + 10} />
